e21dc0fb460f3965e0bd714f187fd110b84abc5f
esraa-hamed <81817985+esraa-hamed@users.noreply.github.com>
2023-12-16 23:31:37 +0200

    Update queries.js

--- src/Borrower_Admin/queries.js
+++ src/Borrower_Admin/queries.js
@@ -4,6 +4,8 @@ const checkEmailExists = "SELECT * FROM borrowers WHERE email = $1 "
 const addBorrower = "INSERT INTO borrowers (name, email, rdate) VALUES ($1, $2, $3)";
 const deleteBorrower = "DELETE FROM borrowers WHERE id = $1";
 const updateBorrower = "UPDATE borrowers SET name = $1, email = $2 WHERE id = $3";
+const checkBorrower = "SELECT * FROM borrowingdetails WHERE ID = $1 ";
+const deleteFromBorrowingDetails = "DELETE FROM borrowingdetails WHERE id = $1";
 
 module.exports = {
     getBorrowers,
@@ -12,4 +14,6 @@ module.exports = {
     addBorrower,
     deleteBorrower,
     updateBorrower,
+    checkBorrower,
+    deleteFromBorrowingDetails,
 }
